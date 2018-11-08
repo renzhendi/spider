11b1dc68b2d83ff85668cc0a63c5cbe566c1f7ce
Der KoD <y.m._wu@hotmail.com>
2018-11-08 22:10:28 +0800

    minor improvement

--- csv_output.m
+++ csv_output.m
@@ -3,6 +3,7 @@
 %%%%%%%%%%%%%
 
 data = data(1:data_length,:);
-xlswrite(excel_name,items)
-xlswrite(excel_name,data,'A2');
+sheet = '大众点评';
+xlswrite(excel_name,items,sheet)
+xlswrite(excel_name,data,sheet,'A2');
 fprintf('全部完成，数据保存在%s表格中。\n',excel_name);
